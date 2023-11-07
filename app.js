5ab7c4005bf1a72b085d710154b0692279902b95
Joel Montesanos <joelmontesanos@MacBook-Air-de-Joel.local>
2023-11-07 12:10:33 -0600

    Fisrt Commit

---
+++ app.js
@@ -0,0 +1,30 @@
+const { createBot, createProvider, createFlow, addKeyword } = require('@bot-whatsapp/bot')
+
+const QRPortalWeb = require('@bot-whatsapp/portal')
+const BaileysProvider = require('@bot-whatsapp/provider/baileys')
+const MockAdapter = require('@bot-whatsapp/database/mock')
+
+
+
+
+const main = async () => {
+    const adapterDB = new MockAdapter()
+    const adapterFlow = createFlow([
+        welcome,
+        headcoach,
+        sellercoach,
+        pagar,
+        costos,
+    ])
+    const adapterProvider = createProvider(BaileysProvider)
+
+    createBot({
+        flow: adapterFlow,
+        provider: adapterProvider,
+        database: adapterDB,
+    })
+
+    QRPortalWeb()
+}
+
+main()
