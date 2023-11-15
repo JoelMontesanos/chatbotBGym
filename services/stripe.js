f419a3bb8f53697d0ed6a9df3d26b16db7da6635
Joel Montesanos <joelmontesanos@MacBook-Air-de-Joel.local>
2023-11-15 14:06:54 -0600

    Fixed: localhost to railway adress

--- services/stripe.js
+++ services/stripe.js
@@ -7,13 +7,13 @@ const handlerStripe = async (nombre = '', horario = '',phone = '', plan='') => {
   let priceId = ''
   switch (plan){
     case '5 Días':  priceId = process.env.DIAS5;
-    //console.log(process.env.DIAS5);
+    console.log(process.env.DIAS5);
     break;
     case '3 Días': priceId = process.env.DIAS3;
-    //console.log(process.env.DIAS3);
+    console.log(process.env.DIAS3);
     break;
     case 'Pareja': priceId = process.env.PAREJA;
-    //console.log(process.env.PAREJA);
+    console.log(process.env.PAREJA);
     break;
     default:
       break; 
@@ -32,8 +32,8 @@ const handlerStripe = async (nombre = '', horario = '',phone = '', plan='') => {
   urlencoded.append("line_items[0][quantity]", "1");
   urlencoded.append("allow_promotion_codes", "false");
   urlencoded.append("customer_creation", "always");
-  urlencoded.append("success_url", `${FRONT_URL}/api/callback?p=${encryptData(`${phone}__success__${plan}__${nombre}__${horario}`)}`);
-  urlencoded.append("cancel_url", `${FRONT_URL}/api/callback?p=${encryptData(`${phone}__fail__${plan}__${nombre}__${horario}`)}`);
+  urlencoded.append("success_url", `chatbotbgym-production.up.railway.app/api/callback?p=${encryptData(`${phone}__success__${plan}__${nombre}__${horario}`)}`);
+  urlencoded.append("cancel_url", `chatbotbgym-production.up.railway.app/api/callback?p=${encryptData(`${phone}__fail__${plan}__${nombre}__${horario}`)}`);
   urlencoded.append("mode", "payment");
 
   const requestOptions = {
