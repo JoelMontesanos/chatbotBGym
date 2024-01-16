ec6541b96658f537a92d35ffce70803c4b189700
Joel Montesanos <joelmontesanos@MacBook-Air-de-Joel.local>
2024-01-15 19:23:33 -0600

    feed: hsbc account added

--- flows/cuentasFlow.js
+++ flows/cuentasFlow.js
@@ -5,7 +5,8 @@ module.exports = addKeyword(['cuentas','Cuentas','Cuenta','cuentas'])
     const currenstate = state.getMyState()
     return flowDynamic(
         [
-            'Ember Servicios Amazo SA de CV *Banregio* Cuenta: _167945860016_ Clabe: _058680000150385021_ RFC: ESA231109DH0'
+            'Ember Servicios Amaso SA de CV *Banregio* Cuenta: _167945860016_ Clabe: _058680000150385021_ RFC: ESA231109DH0',
+            '*HSBC* Cuenta: _4070457668_ Clabe: _021680040704576684_ RFC: ESA231109DH0'
         ]
     ), {delay:2000}
 })
