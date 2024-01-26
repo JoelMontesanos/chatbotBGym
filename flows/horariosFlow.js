cf1bdb63a3b0d4697a83b88612ea94f682b9e053
Joel Montesanos <joelmontesanos@MacBook-Air-de-Joel.local>
2024-01-26 17:28:29 -0600

    Feed: horrarios updated

--- flows/horariosFlow.js
+++ flows/horariosFlow.js
@@ -6,10 +6,10 @@ module.exports = addKeyword(['horarios','horario','ubicados','encuentran', 'info
     return flowDynamic(
         [
             'Nuestros horarios son los siguiente',
-            'Facturación: Lunes a Viernes de 10 am a 8 pm',
-            'Transferencias: Lunes a viernes 10 am a 5:30 pm',
-            'Transferencias internacionales (EURO): 9 am a 12:30 pm',
-            'Transferencias internacionales (US DOLLAR): 9 am a 3:00pm'
+            '*Facturación*: Lunes a Viernes de 10 am a 8 pm',
+            '*Transferencias*: Lunes a viernes 10 am a 5:30 pm',
+            '*Transferencias internacionales (EURO)*: 9 am a 12:30 pm',
+            '*Transferencias internacionales (US DOLLAR)*: 9 am a 3:00pm'
         ]
     ), {delay:1000}
 })
